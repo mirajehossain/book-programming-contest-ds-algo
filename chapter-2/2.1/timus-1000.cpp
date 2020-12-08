09d1019c4670d265f850ca80d5d213facbead3e8
Alamin <miraajehossain@gmail.com>
2020-12-08 11:58:49 +0600

    solved timus 1000

---
+++ chapter-2/2.1/timus-1000.cpp
@@ -0,0 +1,16 @@
+/**
+ * 1000. A+B Problem
+ * input a and b
+ * output a+b
+ *
+ **/
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    cout << a + b << endl;
+}
